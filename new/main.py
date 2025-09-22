63f1afd6399d83e1a30782ad68b6a8407cb47233
vltkac <vladtkachuk135@gmail.com>
2025-09-23 00:34:39 +0200

    initial commit

--- new/main.py
+++ new/main.py
@@ -1,104 +1,86 @@
-# Створіть програму роботи зі словником. Наприклад,
-# англо-іспанський, французько-німецький або інша мовна пара.
-# Програма має:
-#  надавати початкове введення даних для словника
-#  відображати слово та його переклади
-#  дозволяти додавати, змінювати, видаляти переклади
-# слова
+# Я так понял, что по условию задачи каждый узел - объект класса Car.
+# Получается, что можно добавить только один автомобиль этой марки? Если я не так понял задание, дайте фидбэк, пожалуйста.
 
-import bintrees
+from bintrees import BinaryTree
 
 
-class Translation:
-    def __init__(self, word: str, usage_example=None):
-        self.word = word
-        if usage_example:
-            self.usage_example = usage_example
-        else:
-            self.usage_example = ''
+class Car:
+    def __init__(self, brand: str, model: str, year: int):
+        if not isinstance(brand, str) or not isinstance(model, str) or not isinstance(year, int):
+            raise TypeError('Please insert proper type object in the parameter field.')
+        elif year < 1900:
+            raise ValueError('Please insert correct value in the parameter field.')
+
+        self.brand = brand.strip()
+        self.model = model.strip()
+        self.year = year
 
     def __str__(self):
-        return f'Translation: {self.word}, usage example: {self.usage_example}'
-
-class WordsCouples:
-    def __init__(self, word: str):
-        self.word = word
-        self.translations = {}
-        #self.usage_example =
-
-    def add_translation(self, trans_word: str, usage_example=None):
-        this_translation = Translation(trans_word, usage_example)
-
-        self.translations[trans_word] = this_translation
-
-    def delete_translation(self, trans_word: str):
-        self.translations.pop(trans_word)
-
-    def __str__(self):
-        all_translations = ', '.join(self.translations)
-        #all_usage_examples = ', '.join(self.translations.values())
-        all_usage_examples = ''
-
-        for t in self.translations.values():
-            all_usage_examples += str(t) + ', \n'
+        return f'{self.brand} {self.model} {self.year} year'
 
 
-        return f'Word: {self.word}, translations: {all_translations}, usage examples: {all_usage_examples}'
-
-class Dictionary:
+class CarPark:
     def __init__(self):
-        self.words = bintrees.AVLTree()
+        self.cars = BinaryTree()
 
-    def add_word(self, word: str):
-        this_word = WordsCouples(word)
-        self.words.insert(key=word, value=this_word)
+    def __len__(self):
+        return len(self.cars)
 
-    def delete_word(self, word: str):
-        if word in self.words:
-            self.words.remove(key=word)
+    def _brand_check(self, brand):
+        if not brand in self.cars:
+            print(f'No {brand} found in database.\n')
+            return False
+        return True
 
-    def add_translation(self, key_word: str, translation: str, usage_examples=None):
-        if key_word in self.words:
-            self.words[key_word].add_translation(translation)
-            return
+    def add(self, car: Car):
+        if not isinstance(car, Car):
+            raise TypeError('Please insert proper type object in the parameter field.')
 
-        print(f'No {key_word} in the dictionary found.')
+        self.cars.insert(key=car.brand, value=car)
 
-    def delete_translation(self, key_word: str, translation: str):
-        if key_word in self.words:
-            self.words[key_word].delete_translation(translation)
-            return
+        print(f'{car} was added successfully.\n')
 
-        print(f'No {key_word} in the dictionary found.')
+    def search(self, brand: str):
+        if not self._brand_check(brand):
+            return None
 
-    def display_word(self, key_word: str):
-        if key_word in self.words:
-            print(self.words[key_word])
-            return
+        this_car = self.cars[brand]
 
-        print(f'No {key_word} in the dictionary found.')
+        print(f'{this_car} is in the database.\n')
+        return this_car
 
+    def remove(self, brand: str, verbose=True):
+        self._brand_check(brand)
+        self.cars.remove(brand)
 
-dict1 = Dictionary()
+        if verbose:
+            print(f'Car was removed successfully.\n')
 
-dict1.add_word('Apple')
-dict1.add_translation('Apple', 'Яблоко', 'Яблоко упало.')
-dict1.add_translation('Apple', 'Яблочко')
+    def sell_car(self, client: str, brand: str):
+        if not client.isalpha():
+            raise ValueError("Please insert client's name.")
 
-dict1.add_word('Word')
-dict1.add_translation('Word', 'Слово')
+        self._brand_check(brand)
 
-dict1.delete_word('Word')
+        print(f'{self.cars[brand]} was sold to {client}.\n')
 
-dict1.display_word('Apple')
-dict1.display_word('Word')
-
-dict1.delete_translation('Apple', 'Яблочко')
-dict1.display_word('Apple')
+        self.remove(brand, False)
 
 
+car1 = Car('Toyota', 'Corolla', 2025)
+car2 = Car('Porsche', 'Cayenne', 2025)
+car3 = Car('Skoda', 'Octavia', 2020)
 
+car_park = CarPark()
 
+car_park.add(car1)
+car_park.add(car2)
+car_park.add(car3)
 
+car_park.remove('Toyota')
 
+car_park.search('Toyota')
 
+car_park.sell_car('Oleg', 'Porsche')
+
+print(len(car_park))
